832df5ad5781a51cb1aaf41ab6f47643383d57cc
Anthony Bishop <32147014+Axyrll@users.noreply.github.com>
2019-01-31 09:14:28 -0800

    Update In_Class.c

--- In_Class.c
+++ In_Class.c
@@ -2,18 +2,20 @@
 
 
 
-float Vin;
-
-
-void setup(){
+void setup(void){
 
 
 }
 
 
+int pot_pin;
+int display_pin;
+
+
+float Vin = 0; //voltage value to be read from divider
+float value_out = 0; //value to be passed to display
 
 void loop(){
-
 
 
 
